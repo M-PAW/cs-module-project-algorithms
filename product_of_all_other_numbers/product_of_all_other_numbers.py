e4f3862407003239266b628a95d786a1a0f7491d
Michael Walter <michael@MacBook-Pro.local>
2020-06-18 16:06:45 -0600

    P_o_a_o_nums complete and tests passing.

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -5,7 +5,21 @@ Returns: a List of integers
 def product_of_all_other_numbers(arr):
     # Your code here
 
-    pass
+    # Create a new list to hold all numbers except the one at the specific index
+    newList = []
+
+    # Iterate through the list
+    for index, item in enumerate(arr):
+        # Create new list that holds all except th one at the index
+        other_nums = [x for i, x in enumerate(arr) if i != index]
+        product = 1
+
+        # Iterate through the numbers and multiply them together 
+        for n in other_nums:
+            product = product * n
+        # Append the results to the new list
+        newList.append(product)
+    return newList
 
 
 if __name__ == '__main__':
